0fdaeac3e8810b2df9cf328ab70403a4d3a6f87e
Emanuel Rojas <eroaguero01@gmail.com>
2022-08-09 19:45:42 -0600

    Cambio en las funciones, eliminado de funcion de usuario, unificada en la de login

--- Proyecto_Calidad_Del_Software.py
+++ Proyecto_Calidad_Del_Software.py
@@ -14,18 +14,11 @@ def start_connection():
     return driver
 
 
+
+
 def test_netflix_login(driver):
     driver.implicitly_wait(0.5)
     driver.find_element_by_link_text('Sign In').click()
-    driver.find_element_by_id('id_userLoginId').send_keys(config.username)
-    driver.find_element_by_id('id_password').send_keys(config.password)
-    driver.find_element_by_xpath('//button[@type="submit"]').click()
-    driver.implicitly_wait(0.5)
-
-
-def test_netflix_user_selection(driver):
-    driver.implicitly_wait(0.5)
-    driver.find_element_by_link_text('Sign In').click()
     driver.implicitly_wait(0.5)
     driver.find_element_by_id('id_userLoginId').send_keys(config.username)
     driver.implicitly_wait(0.5)
@@ -35,36 +28,30 @@ def test_netflix_user_selection(driver):
     driver.implicitly_wait(0.5)
     driver.find_element_by_link_text(config.profile).click()
     driver.implicitly_wait(0.5)
-    test_netflix_logout(driver)
-    driver.implicitly_wait(0.5)
+    
 
 def test_netflix_search(driver):
     driver.implicitly_wait(0.5)
-    driver.find_element_by_link_text('Sign In').click()
-    driver.find_element_by_id('id_userLoginId').send_keys(config.username)
-    driver.find_element_by_id('id_password').send_keys(config.password)
-    driver.find_element_by_xpath('//button[@type="submit"]').click()
-    driver.implicitly_wait(0.5)
-    driver.find_element_by_link_text(config.profile).click()
+    test_netflix_login(driver)
     driver.implicitly_wait(0.5)
     driver.find_element_by_xpath('//button[@class="searchTab"]').click()
     driver.implicitly_wait(1.5)
     driver.find_element_by_id("searchInput").send_keys("Suits")
     driver.implicitly_wait(1.5)
-    driver.find_element_by_xpath('//button[@class="searchButton"]').click()
-    driver.implicitly_wait(1.5)
+    
 
 def test_netflix_logout(driver):
     driver.implicitly_wait(0.5)
-    driver.find_element_by_class_name('caret').click()
+    test_netflix_login(driver)
+    driver.find_element(By.CLASS_NAME, "caret").click()
     driver.implicitly_wait(0.5)
-    driver.find_element_by_link_text('Cerrar sesion en Netflix').click()
+    driver.find_element(By.CLASS_NAME('sub-menu-link st-hover')).click()
     driver.implicitly_wait(0.5)
-    driver.back()
+    driver.close()
 
 
 
 
 test_netflix_login(start_connection())
 test_netflix_search(start_connection())
-test_netflix_user_selection(start_connection())
+test_netflix_logout(start_connection())
